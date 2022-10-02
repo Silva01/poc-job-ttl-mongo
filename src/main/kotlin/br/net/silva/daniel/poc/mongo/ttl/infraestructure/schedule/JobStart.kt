f321fbf6691c564cb24a832ff67ab752242122cf
silva01 <silva20102@gmail.com>
2022-10-02 00:02:25 -0300

    AJustes para execução

--- src/main/kotlin/br/net/silva/daniel/poc/mongo/ttl/infraestructure/schedule/JobStart.kt
+++ src/main/kotlin/br/net/silva/daniel/poc/mongo/ttl/infraestructure/schedule/JobStart.kt
@@ -3,6 +3,7 @@ package br.net.silva.daniel.poc.mongo.ttl.infraestructure.schedule
 import br.net.silva.daniel.poc.mongo.ttl.domain.scheduling.dto.SchedulingDTO
 import br.net.silva.daniel.poc.mongo.ttl.domain.scheduling.factory.SchedulingFactory
 import br.net.silva.daniel.poc.mongo.ttl.infraestructure.component.SchedulingComponent
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -12,13 +13,20 @@ import java.util.UUID
 @Component
 class JobStart(@Autowired private val component: SchedulingComponent) {
 
+    private val log = LoggerFactory.getLogger("JobStart")
+
     @Scheduled(cron = "0 0/1 * * * *")
     fun start(): Unit {
-        println("Iniciado")
+        log.info("Iniciando Job")
+        log.info("Gerando registro para ser gravado no mongoDB")
         val userCase = SchedulingFactory.instanceSchedulingUserCase(component)
+
+        log.info("Gerando UUID")
         val uuid = UUID.randomUUID()
         val dto = SchedulingDTO(uuid.toString(), "Bolivon-${uuid}", LocalDateTime.now())
+
+        log.info("Gravando dados na base de dados")
         userCase.toRecord(dto)
-        println("Finalizado")
+        log.info("Job encerrado com sucesso")
     }
 }
